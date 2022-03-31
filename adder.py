9f5d4e05b9989c94c99459db438dbd77af66df00
sidhaniedotcom <102436523+sidhaniedotcom@users.noreply.github.com>
2022-03-31 17:07:07 +0700

    Update adder.py

--- adder.py
+++ adder.py
@@ -20,11 +20,11 @@ print (gr+" ║ ├┤ │  ├┤ ║ ╦  ╠═╣ ││ ││├┤ ├┬
 print (re+" ╩ └─┘┴─┘└─┘╚═╝  ╩ ╩─┴┘─┴┘└─┘┴└─")
 
 print (cy+"version : 1.01")
-print (cy+"Make sure you Subscribed Termux Professor On Youtube")
-print (cy+"www.youtube.com/c/TermuxProfessorYT")
+print (cy+"Join Channel Telegram Sidhanie")
+print (cy+"https://t.me/termuxsidhanie")
 
 print (re+"NOTE :")
-print ("1. Telegram only allow to add 200 members in group by one user.")
+print ("1. Telegram only allow to add 2000 members in group by one user.")
 print ("2. You can Use multiple Telegram accounts for add more members.")
 print ("3. Add only 50 members in group each time otherwise you will get flood error.")
 print ("4. Then wait for 15-30 miniute then add members again.")
